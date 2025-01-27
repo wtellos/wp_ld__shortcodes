51c57327bcb30dd19adfcc75616e6902bc0d2640
WTellos <31623201+wtellos@users.noreply.github.com>
2025-01-27 12:25:54 +0200

    Add do_shortcode for RISE Return button - plugin.php

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+function single_lesson_function($course_id) {
+
+  ///// CODE CHANGE TO do_shortcode in order to print text correctly
+<?php echo do_shortcode('[cardet_return_shortcode]'); ?>
+
+  // RESULT:
+
+<a class="uk-button uk-button-primary return-rise-button"><?php echo do_shortcode('[cardet_return_shortcode]'); ?></a><span style="z-index:1;" class="uk-position-center cardet-modal-spinner" uk-spinner="ratio: 4.5"></span></div>
+</div>
+</div>
+  
